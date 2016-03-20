c63ac52c18db27c5d0cb4a1617389267bcd8aa35
Luis Felipe Sousa <rumpertumpski@gmail.com>
2016-03-20 20:28:27 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+	use \Psr\Http\Message\ServerRequestInterface as Request;
+	use \Psr\Http\Message\ResponseInterface as Response;
+
+	require 'vendor/autoload.php';
+
+	$c = new \Slim\Container();
+
+	$c["notFoundHandler"] = function ($c) {
+	    return function ($req, $res) use ($c) {
+	        return $c["response"]->withRedirect($c["request"]->getUri()->getBasePath());
+	    };
+	};
+
+	$app = new \Slim\App($c);
+
+	$app->add(function($req, $res, $next) {
+		$res = $res
+			->withAddedHeader("Content-type", "application/json;utf-8")
+			->withAddedHeader("Access-Control-Allow-Origin", "*");
+		return $next($req, $res);
+	});
+
+	$app->get("/", "index");
+
+	function index($req, $res) {
+	    echo "{\"success\":true}";
+	    return $res;
+	}
+
+	$app->run();
+	
+?>
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Simple Rest API PHP
+
+## Description
+
+Simple Rest API example. Featured:
+
+* Composer
+* Slim Framework 3
+
+## Quick Install
+
+1. _composer install_
+2. serve
+3. Happy coding!
