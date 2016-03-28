5132fb5a9e2e3a323378eef5c5574f796aa6d41b
Luis Felipe Sousa <rumpertumpski@gmail.com>
2016-03-28 12:53:14 -0300

    Add Eloquent example

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 <?php
 
+	require "vendor/autoload.php";
+
 	use \Psr\Http\Message\ServerRequestInterface as Request;
 	use \Psr\Http\Message\ResponseInterface as Response;
+	use \Illuminate\Database\Capsule\Manager as DBManager;
+	use \Illuminate\Database\Eloquent\Model;
 
-	require "vendor/autoload.php";
+	/*
+	# EXAMPLE DATABASE CONNECTION
+	$manager = new DBManager;
+	$manager->addConnection(array(
+		"driver" => "mysql",
+		"host" => "localhost",
+		"database" => "library",
+		"username" => "root",
+		"password" => "",
+		"charset" => "utf8",
+		"collation" => "utf8_unicode_ci",
+		"prefix" =>""
+	));
+	$manager->bootEloquent();
+	*/
+
+	/*
+	# EXAMPLE DATABASE "FIND BY ID"
+	class User extends Model {
+		protected $table = "user";
+	}
+	$user = User::find(1);
+	print_r($user);
+	*/
 
 	$c = new \Slim\Container();
 
